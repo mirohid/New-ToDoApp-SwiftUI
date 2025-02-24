b870875bd4f336659191c43aa64d8dc9df19ee4c
mirohid <mirohid7860@gmail.com>
2025-02-24 15:09:15 +0530

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by MacMini6 on 24/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
